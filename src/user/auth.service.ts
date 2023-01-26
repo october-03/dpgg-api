84767d21d07c3ea18d1d167f657df2bdf41ecb8b
october-03 <october03@o365sen.net>
2023-01-26 15:19:37 +0900

    유저 정보 인증 기능 개발

--- src/user/auth.service.ts
+++ src/user/auth.service.ts
@@ -1,19 +1,46 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { registerUserDto } from 'src/dto/user.dto';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import { UserService } from './user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
+    private userService: UserService,
   ) {}
 
   async register(req: registerUserDto) {
     const newUser: User = this.usersRepository.create({ ...req });
     await this.usersRepository.save(newUser);
     return newUser;
+  }
+
+  async verifyPassword(password, hashedPassword) {
+    const isMatch = await bcrypt.compare(password, hashedPassword);
+    if (!isMatch) {
+      throw new HttpException(
+        '인증 정보가 잘못되었습니다.',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  async getAuthUser(email: string, password: string) {
+    try {
+      const user: User = await this.userService.findOne(email);
+      await this.verifyPassword(password, user.password);
+      user.password = undefined;
+      return user;
+    } catch (err) {
+      throw new HttpException(
+        '인증 정보가 잘못되었습니다.',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 }
